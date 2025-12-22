8789b577c77aa48142591c72be477b9a79f840f5
aparnamg <aparnamg2696@gmail.com>
2025-12-22 22:37:37 +0400

    Added product catalogue models, admin CRUD, and media handling

--- kiddora/accounts/admin.py
+++ kiddora/accounts/admin.py
@@ -1,33 +1,62 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, EmailOTP
+from .models import CustomUser
 
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
 
+    # Display
     list_display = (
-        'username', 'email', 'is_staff', 'is_active',
-        'is_blocked', 'date_joined'
+        'username',
+        'email',
+        'is_staff',
+        'is_active',
+        'is_blocked',
+        'date_joined',
     )
 
-    list_filter = ('is_staff', 'is_active', 'is_blocked')
-    search_fields = ('username', 'email', 'first_name', 'last_name')
+    # Search
+    search_fields = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+    )
+
+    # Filters
+    list_filter = (
+        'is_staff',
+        'is_active',
+        'is_blocked',
+        'date_joined',
+    )
+
+    # Sorting
     ordering = ('-date_joined',)
+
+    # Pagination
     list_per_page = 25
 
+    # Bulk actions
     actions = ['block_users', 'unblock_users']
 
+    # Custom fields in admin edit page
     fieldsets = UserAdmin.fieldsets + (
         (
-            'Additional Info',
+            'Profile Details',
             {
                 'fields': (
-                    'profile_image', 'phone',
-                    'address_line1', 'address_line2',
-                    'city', 'state', 'postal_code', 'country',
-                    'is_blocked'
+                    'profile_image',
+                    'phone',
+                    'address_line1',
+                    'address_line2',
+                    'city',
+                    'state',
+                    'postal_code',
+                    'country',
+                    'is_blocked',
                 )
             },
         ),
@@ -41,9 +70,3 @@ class CustomUserAdmin(UserAdmin):
 
     block_users.short_description = "Block selected users"
     unblock_users.short_description = "Unblock selected users"
-
-
-@admin.register(EmailOTP)
-class EmailOTPAdmin(admin.ModelAdmin):
-    list_display = ('user', 'otp', 'is_verified', 'created_at')
-    ordering = ('-created_at',)
